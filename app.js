09f52692c3fd42e41620cbe979ad5d56a0a4bf69
polvora-fosforo <contaparacodigo999@gmail.com>
2020-09-17 11:32:12 -0300

    Inicializando a estratégia de login (ainda sem o JWT)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+// importando a estrategia de autenticacao feita no commit anterior.
+const { estrategiasAutenticacao } = require('./src/usuarios');
 
 app.use(
   bodyParser.urlencoded({
--- src/usuarios/usuarios-rotas.js
+++ src/usuarios/usuarios-rotas.js
@@ -1,6 +1,14 @@
 const usuariosControlador = require('./usuarios-controlador');
+const passport = require('passport');
 
 module.exports = app => {
+  app
+    .route('/usuario/login')
+    .post(
+      // esse método da autenticação irá executar nossa estratégia definida no commit anterior
+      passport.authenticate('local', { session: false }),
+      usuariosControlador.login
+    );
   app
     .route('/usuario')
     .post(usuariosControlador.adiciona)
